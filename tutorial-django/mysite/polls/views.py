2393c194a00bfc607231c314fcb9e72b8810a65f
Mayza de Oliveira <mayza@mac143880.glb.com>
2011-08-24 16:25:47 -0300

    editando a view e trabalhando com o template

--- tutorial-django/mysite/polls/views.py
+++ tutorial-django/mysite/polls/views.py
@@ -4,11 +4,13 @@ from polls.models import Poll
 from django.http import HttpResponse
 
 def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
+    return render_to_response('polls/detail.html', {'poll': p},
+                            context_instance=RequestContext(request))
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)

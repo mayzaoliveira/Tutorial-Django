ef7e8fd3f345c2fb5b5fb44ab932b7ba7bb253e0
Mayza de Oliveira <mayza@mac143880.glb.com>
2011-08-24 16:24:52 -0300

    editando urls

--- tutorial-django/mysite/urls.py
+++ tutorial-django/mysite/urls.py
@@ -1,16 +1,9 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('polls.views',
-    (r'^polls/$', 'index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
-)
-
-urlpatterns += patterns('',
-  url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('',
+    (r'^polls/', include('polls.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
